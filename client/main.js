3a1af83ee148b911b71a05856aced800668dec27
i25959341 <karlson.lee09@gmail.com>
2017-02-05 13:53:06 +0000

    finished splash page and transtion to navbar

--- client/main.js
+++ client/main.js
@@ -1,1 +1,17 @@
 
+if (Meteor.isClient) {
+    $(function() {
+      $(document).ready(function() {
+        $(window).scroll(function() {
+          console.log($(window).scrollTop())
+          if ($(window).scrollTop() > 1000) {
+            $('.navbar').addClass('navbar-fixed-top');
+            $('.dummy-div').remove();
+          }
+          if ($(window).scrollTop() < 1001) {
+            $('.navbar').removeClass('navbar-fixed-top');
+          }
+        });
+        });
+    });
+}
